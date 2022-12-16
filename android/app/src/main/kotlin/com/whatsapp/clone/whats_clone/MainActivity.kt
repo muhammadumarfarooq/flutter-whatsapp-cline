6bace6ae425c1f6b3558a02436747dc204054334
umar <umar@craveup.com>
2022-12-16 10:02:04 +0500

    basic setup

---
+++ android/app/src/main/kotlin/com/whatsapp/clone/whats_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.whatsapp.clone.whats_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:whats_clone/my_app.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const MyApp(),
+  ));
+}
